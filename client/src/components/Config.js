8948223c3d7f63a8bd47595e13dc668961e9f565
Jung hongki <82.hongki.jung@gmail.com>
2021-10-04 19:25:38 +0900

    서버 배포

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,3 +1,3 @@
 //SERVER ROUTES
-export const USER_SERVER = 'http://localhost:4000';
-// export const USER_SERVER = 'https://api.soso-ceramic-studio.com'
+// export const USER_SERVER = 'http://localhost:4000';
+export const USER_SERVER = 'https://api.soso-ceramic-studio.com'
